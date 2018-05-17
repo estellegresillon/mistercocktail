a6f2b010ea94901602c3b9aa55af8e2c68d5d215
estellegresillon <estelle.gresillon@gmail.com>
2018-05-17 13:41:41 +0200

    almost done

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+  before_action :dose_params, only: :create
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail, notice: "Dose added !"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
